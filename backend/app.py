dea212f2e461b116b030373dfe7c0536397f15f7
sirisha-fulara <sirishaf2005@gmail.com>
2026-02-25 23:57:56 +0530

    change in reqs

--- backend/app.py
+++ backend/app.py
@@ -13,9 +13,7 @@ from routes.auth import auth_bp
 from routes.quiz import quiz_bp
 from sockets.quiz_socket import *
 
-
 port = int(os.environ.get("PORT", 5000))
-
 
 socketio= SocketIO(cors_allowed_origins="*",
                    async_mode="eventlet")
