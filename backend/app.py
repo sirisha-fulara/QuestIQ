00035a40da1140d2e41a5731d95d86572d47846a
sirisha-fulara <sirishaf2005@gmail.com>
2026-02-26 23:29:24 +0530

    change

--- backend/app.py
+++ backend/app.py
@@ -37,4 +37,4 @@ def create_app():
 
 app= create_app()
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=port)
+    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
