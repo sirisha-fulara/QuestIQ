b25da7b60165e38529b6b539e13742ed41515612
sirisha-fulara <sirishaf2005@gmail.com>
2026-02-26 23:04:36 +0530

    frotnend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API= axios.create({baseURL: "http://127.0.0.1:5000"})
+const API= axios.create({baseURL: process.env.REACT_APP_API_BASE_URL})
 
 API.interceptors.request.use(config=>{
     const token= localStorage.getItem('token')
