eb3f4a2a463d7819871b37b09e8a76f49a19b763
sirisha-fulara <sirishaf2005@gmail.com>
2026-03-04 14:34:33 +0530

    login fallback

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,13 +1,31 @@
 import axios from 'axios'
 
-const API= axios.create({baseURL: process.env.REACT_APP_API_BASE_URL})
-
-API.interceptors.request.use(config=>{
-    const token= localStorage.getItem('token')
-    if(token){
-        config.headers.Authorization= `Bearer ${token}`
-    }
-    return config
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE_URL
 })
 
-export default API;
+API.interceptors.request.use(config => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+API.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response && error.response.status === 401) {
+
+      alert("Session expired. Please login again.")
+
+      localStorage.removeItem("token")
+
+      window.location.href = "/login"
+    }
+
+    return Promise.reject(error)
+  }
+)
+
+export default API
